6ff4cffe7c4a3cd6939439d97e4df31b686a32a5
Mohd Noman <9319141719noman@gmail.com>
2026-09-21 01:19:04 +0530

    connect database

---
+++ src/index.js
@@ -0,0 +1,40 @@
+//require('dotenv').config({path: './env'})
+import dotenv from "dotenv" // provide env file to all
+import mongoose from "mongoose"
+import { DB_NAME } from "./constants.js"
+import connectDB from "./db/index.js"
+
+
+dotenv.config({
+    path: './.env'
+})
+
+// In this method we make another folder/file for mongodb and call it in index.js
+connectDB()
+
+
+
+
+
+
+/*
+In this approach we call mongodb in the index file 
+import express from "express"
+const app = express()
+(async ()=>{   // this iife's normally functions are executed when called. iife's allow functions to execute immediately
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error",(error)=>{
+            console.log("ERROR ",error);
+            throw error 
+        })
+        app.listen(process.env.PORT, ()=>{
+            console.log(`App is listening on port ${process.env.PORT}`);
+        })
+    }
+    catch(error){
+        console.log("ERROR ",error)
+        throw error
+    }
+})()
+*/
